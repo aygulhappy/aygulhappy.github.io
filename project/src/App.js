bbbf1714501f2369b32a1a9cb454f89c7aa232d2
Азат Аюпов <aiupov.a@clover.global>
2021-07-17 19:56:37 +0300

    publish

---
+++ project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './images/logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img style={{ borderRadius: '50%' }} src={logo} className="App-logo" alt="logo" />
+        <p style={{ color: 'pink' }}>Aygul</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
